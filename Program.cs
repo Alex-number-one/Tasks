f6604905ba8fa32c2d21aaba2afd31307d1792da
Alex <aleksejtatukov098@gmail.com>
2023-06-23 22:36:25 +0300

    the second

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿int a = Convert.ToInt16(Console.ReadLine());
+int b = Convert.ToInt16(Console.ReadLine());
+int c = Convert.ToInt16(Console.ReadLine());
+int max = a;
+if (b > max)
+{
+    max = b;
+}
+else
+{
+    max = a;
+}
+if (c > max)
+{
+    max = c;
+    Console.WriteLine("max = " + max);
+}
+else
+{
+Console.WriteLine("max = " + max);
+}
