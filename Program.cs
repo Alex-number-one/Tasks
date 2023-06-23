7611ae3114e4ed2daacd98a74113fc278d1aea89
Alex <aleksejtatukov098@gmail.com>
2023-06-23 15:11:39 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿//double num = Convert.ToDouble(Console.ReadLine);
+/*int first = Convert.ToInt32(Console.ReadLine());
+int second = Convert.ToInt32(Console.ReadLine());
+if (first == second * second) //before {} never goes ;
+{
+Console.WriteLine("Yes");
+}
+else
+{
+Console.WriteLine("No");
+}*/
+Console.Write("write number: ");
+
+int N = Convert.ToInt32(Console.ReadLine());
+if (N < 0)
+{
+    N = N * -1;
+}
+int negativeN = N * -1;
+while (negativeN <= N)
+{
+    Console.WriteLine(negativeN);
+negativeN++;
+}
